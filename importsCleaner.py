338282cb72ac803d56014da10f419b2dcb73294a
ramziChbl <ramzy.chbl@gmail.com>
2021-05-05 17:42:52 +0100

    can now extract multilevel imports

--- importsCleaner.py
+++ importsCleaner.py
@@ -1,24 +1,96 @@
-import re
+import re, ast
 import argparse
+import pprint
+
 
 parser = argparse.ArgumentParser(description='Show unused and repeated imports.')
-parser.add_argument('filesPath', type=str, nargs='+',
-                    help='files to clean')
+parser.add_argument('filesPath', type=str, nargs='+', help='files to clean')
 
 programArgs = parser.parse_args()
 
 for filePath in programArgs.filesPath:
-	print(filePath)
-	with open(filePath, 'r') as browsedFile:
-		for line in browsedFile:
-			#print(line)
-			#print(type(line))
-			if line == '\n':
-				continue
-			print(line, end='')
-	
+    print(filePath)
+    with open(filePath, 'r') as browsedFile:
+        #moduleDict = {}
+        importedModules = {}
+        for line in browsedFile:
 
-#print(programArgs)
+            # Remove leading and trailing characters like : ' ' and '\n'
+            line = line.strip()
 
-#for file in programArgs:
-#	print(file)
+            # Match "import *"
+            #matches = re.search(r'\s*import\s+(\w+\s*,?\s*)+', line)
+            #if matches:
+            #   print(matches.groups())
+            #matches = re.findall(r'\s*import\s+((\w+)\s*,?\s*)+', line)
+            #matches = re.findall(r'\s*import\s+((\w+)\s*,?\s*)+', line)
+            #'(?m)^(?:from[ ]+(\S+)[ ]+)?import[ ]+(\S+)[ ]*$'
+            #if matches:
+        #       print(matches)
+
+            
+            for node in ast.iter_child_nodes(ast.parse(line)):
+                
+                if isinstance(node, ast.ImportFrom):
+                    print('ImportFrom')
+                    print(vars(node))
+                    print(len(node.names))
+                    for n in node.names:
+                        print(vars(n))
+                    if not node.names[0].asname:  # excluding the 'as' part of import
+                        modules.append(node.module)
+                elif isinstance(node, ast.Import): # excluding the 'as' part of import
+                    print('Import')
+                    print(vars(node))
+                    print(len(node.names))
+
+                    for nodeName in node.names:
+                        print(vars(nodeName))
+                        levelNames = nodeName.name.split('.')
+                        print(levelNames)
+                        if len(levelNames) == 1:
+                            if nodeName.name not in importedModules:
+                                moduleDict = {
+                                    'asname' : nodeName.asname,
+                                    'refcount' : 1,
+                                    'subs' : []
+                                }
+                                importedModules[nodeName.name] = moduleDict
+                        else: # Multilevel import eg: matplotlib.pyplot
+                            print('Multilevel Import-----------------------')
+                            # Insert ancestor if doesn't exist
+                            ancestorName = levelNames[0]
+                            if ancestorName not in importedModules:
+                                moduleDict = {
+                                    'asname' : None,
+                                    'refcount' : 0,
+                                    'subs' : []
+                                    }
+                                importedModules[ancestorName] = moduleDict
+
+                            parentDict = importedModules[ancestorName]
+                            moduleDict = {}
+                            for currentModule in levelNames[1:]:
+                                if currentModule not in parentDict:
+                                    moduleDict = {
+                                    'asname' : None,
+                                    'refcount' : 0,
+                                    'subs' : []
+                                    }
+                                    parentDict['subs'].append({currentModule: moduleDict})
+                                else:
+                                    moduleDict = parentDict[currentModule]
+                                    pass
+                                parentDict = moduleDict
+                            moduleDict['refcount'] += 1
+
+
+                            #print(currentImport)
+                    #for n in node.names:
+                    #    print(vars(n))
+                    #if not node.names[0].asname:
+                    #    modules.append(node.names[0].name)
+                print()
+            
+        pprint.pprint(importedModules)
+    
---
+++ testfile.py
@@ -0,0 +1,6 @@
+## Import dependencies
+#from sklearn.metrics import recall_score, classification_report, auc, roc_curve
+#import os
+#import re, argparse as ap
+import numpy as np
+import matplotlib.pyplot as plt
